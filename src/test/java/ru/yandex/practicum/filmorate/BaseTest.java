e870db7fb9ca9f412df607bf54d54cfe9df7412a
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-02-23 23:33:08 +0300

    Fix import in BaseTest

--- src/test/java/ru/yandex/practicum/filmorate/BaseTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/BaseTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 
 public class BaseTest {
     protected static final Logger LOGGER = LoggerFactory.getLogger("test logger");
