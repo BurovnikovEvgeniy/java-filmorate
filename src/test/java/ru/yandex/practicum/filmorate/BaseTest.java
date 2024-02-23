f2bc29c4aeec47f4069824b4ad9eca2ff89e3813
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-02-23 23:34:16 +0300

    Delete BaseTest

--- src/test/java/ru/yandex/practicum/filmorate/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class BaseTest {
-    protected static final Logger LOGGER = LoggerFactory.getLogger("test logger");
-}
