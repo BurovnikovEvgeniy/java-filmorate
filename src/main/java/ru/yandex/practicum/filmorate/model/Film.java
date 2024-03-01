3309d011866ddf261102010d245ba7d00ce7bc59
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-03-01 18:59:31 +0300

    Fix imports

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
